03dd39fbe28ead2b699b574bc4fbb630951995c0
jade0819 <break0819@gmail.com>
2023-09-08 20:38:33 +0900

    Chore: react-icons 설치 및 UI 적용

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,6 +1,11 @@
+import { styled } from 'styled-components';
+import { colors } from '../../constants/colors';
 import { useEffect } from 'react';
 import useSuggestion from '../../hooks/useSuggestion';
 import SuggestionList from './SuggestionList';
+import SearchInput from './SearchInput';
+import Button from '../ui/Button';
+import { BsXCircleFill } from 'react-icons/bs';
 
 const Search = () => {
   const [state, dispatch, fetchSuggestion] = useSuggestion();
@@ -10,13 +15,52 @@ const Search = () => {
     fetchSuggestion('암');
   }, []);
 
+  if (error) return <p>error!</p>;
+  if (isLoading) return <p>검색어 없음</p>;
+
   return (
-    <div>
-      {error && <p>error!</p>}
-      {isLoading && <p>검색어 없음</p>}
-      {!(isLoading || error) && <SuggestionList datas={datas} />}
-    </div>
+    <SearchWrraper>
+      <SearchInner>
+        <SearchContainer>
+          <SearchInput />
+          <KeywordClearBtn />
+          <Button />
+        </SearchContainer>
+        <SuggestionList datas={datas} />
+      </SearchInner>
+    </SearchWrraper>
   );
 };
 
 export default Search;
+
+const SearchWrraper = styled.div`
+  width: 100%;
+  padding: 20px 0;
+`;
+const SearchInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SearchContainer = styled.div`
+  max-width: 490px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 12px;
+  border-radius: 42px;
+  background-color: ${colors.white};
+`;
+
+const KeywordClearBtn = styled(BsXCircleFill)`
+  width: 22px;
+  height: 22px;
+  margin: 0 8px;
+  color: ${colors.gray};
+  cursor: pointer;
+`;
---
+++ src/components/search/SearchInput.jsx
@@ -0,0 +1,5 @@
+const SearchInput = () => {
+  return <div>검색</div>;
+};
+
+export default SearchInput;
