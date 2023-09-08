db64c8690a298dc2c6aae5860f715bda6602e60c
jade0819 <break0819@gmail.com>
2023-09-08 20:53:50 +0900

    Design: 검색 버튼 스타일 적용

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,5 +1,28 @@
+import { styled } from 'styled-components';
+import { colors } from '../../constants/colors';
+import { FaSearch } from 'react-icons/fa';
+
 const Button = () => {
-  return <div>버튼</div>;
+  return (
+    <SearchBtn>
+      <SearchIcon />
+    </SearchBtn>
+  );
 };
 
 export default Button;
+
+const SearchBtn = styled.button`
+  width: 38px;
+  height: 38px;
+  background-color: ${colors.primary};
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+`;
+
+const SearchIcon = styled(FaSearch)`
+  width: 20px;
+  height: 20px;
+  color: ${colors.white};
+`;
